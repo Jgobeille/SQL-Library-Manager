9510e6a3393d0c789510f1000f0daf7594635a19
Jamie Gobeille <djent34@gmail.com>
2020-05-13 18:37:59 +0900

    fixed formatting issues and changed db to library

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,10 @@
 const express = require('express');
+
 const router = express.Router();
 
 /* GET home page. */
-router.get('/', (req, res, next) => {
+// eslint-disable-next-line no-unused-vars
+router.get('/', (req, res, _next) => {
   res.redirect('/books');
 });
 
